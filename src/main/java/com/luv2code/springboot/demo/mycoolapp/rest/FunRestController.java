07b54ce16858b5ff87c265fe308d9cd89d3ff3df
juanmiguel431 <juanmiguel431@gmail.com>
2024-06-17 20:25:59 -0400

    Fun Controller - Reading from application.properties.

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,13 +1,17 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 
+    @Value("${owner.name}")
+    private String ownerName;
+
     @GetMapping("/")
     public String index() {
-        return "Hello World";
+        return "Hello World " + ownerName;
     }
 }
